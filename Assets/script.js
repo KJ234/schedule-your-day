1e210f53368f4e8f929b9ad29af716508a880df2
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-01-22 00:55:28 +0000

    creating a function to change the timeblock colour

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,10 @@
 var time = moment();
 $("#currentDay").text(time.format("dddd, MMMM Do YYYY, H:mm a"));
+
+function changeColour() {
+  var currentHour = moment().hours();
+
+  $(".time-block").each(function () {
+    var newHour = parseInt($(this).attr("id").split("hour")[1]);
+  });
+}
